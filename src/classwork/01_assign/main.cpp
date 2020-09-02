d6d7290a2a64f84f321995e7fc1330f79b73e7b5
a1768618 <abner.herrera-chinea@g.austincc.edu>
2020-09-01 19:02:54 -0500

    Intro to functions, types and test cases.

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,11 +1,12 @@
 //Write the include statement for types.h here
-
+#include "types.h"
 //Write include for capturing input from keyboard and displaying output to screen
-
+#include<iostream>
 
 
 int main() 
 {
+	std::cout<<sum_numbers(5,5);
 
 	return 0;
 }
