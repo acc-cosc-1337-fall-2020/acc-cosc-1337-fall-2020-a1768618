621af777f3094cdd0537ee6c88208be470cedd7a
a1768618 <abner.herrera-chinea@g.austincc.edu>
2020-09-14 20:31:55 -0500

    Worked data types, and memory.

--- src/examples/01_module/06_char/main.cpp
+++ src/examples/01_module/06_char/main.cpp
@@ -1,9 +1,16 @@
+#include<iostream>
 
+using std::cout;
 
 int main() 
 {
 	//Create a char variable and display it
 		
+	char letter = 'a';
+	cout<<"Letter is: "<<letter<<"\n";
+
+	bool flag = false;
+	cout<<"flag is: "<<flag;
 
 	return 0;
 }
