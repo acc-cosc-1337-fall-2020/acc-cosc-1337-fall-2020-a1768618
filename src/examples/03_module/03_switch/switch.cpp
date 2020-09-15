4cfa7d48aebcf35ce54635cd8b3060d949cea848
a1768618 <abner.herrera-chinea@g.austincc.edu>
2020-09-15 18:19:28 -0500

    Switch & immediate if.

--- src/examples/03_module/03_switch/switch.cpp
+++ src/examples/03_module/03_switch/switch.cpp
@@ -1,5 +1,6 @@
 //write include statement for switch header
 #include "switch.h"
+#include <string>
 
 //Write code for function that accepts a num and returns a string 
 //as follows:
@@ -8,4 +9,33 @@
 //returns 'Option 3' when value 3
 //returns 'Option 4' when value 4
 //return 'Invalid Option' otherwise
+
+std::string menu(int option)
+{
+    std::string return_val;
+
+    switch(option)
+    {
+    case 1:
+        return_val = "Option 1";
+        break;
+    case 2:
+        return_val = "Option 2";
+        break;
+    case 3:
+        return_val = "Option 3";
+        break;
+    case 4:
+        return_val = "Option 4";
+        break;
+            
+    default:
+        return_val = "Invalid Option";
+
+    }
+
+    return return_val;
+
+}
+    
 
