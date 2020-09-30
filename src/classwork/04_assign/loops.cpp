caa59fe2b47d926b1459265b4800316eb641d037
a1768618 <abner.herrera-chinea@g.austincc.edu>
2020-09-30 16:34:12 -0500

    Completed HW 3

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,1 +1,19 @@
+// This program calculates the factorial of a number
+// that the user inputs.
+
 //cpp
+#include <iostream>
+
+using std::cout;    using std::cin;
+
+int factorial(int num)
+{
+    // Declare and initialize variables
+    int fact = 1;
+
+    for(int count = 1; count <= num; ++count)
+    {
+        fact = fact * count;
+    }
+    return fact;
+}
--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,7 +1,9 @@
 //write includes statements
+#include "loops.h"
+#include<iostream>
 
 //write using statements for cin and cout
-
+using std::cout;    using std::cin;
 
 /*
 Use a do while loop to prompt the user for 
@@ -10,6 +12,28 @@ factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
 {
+	int num;
+	char repeat;
+
+	do
+	{
+		cout << "Please enter a number: ";
+		cin >> num;
+
+		while(num < 1 || num > 10)
+		{
+			cout << "Please enter a number between 1 and 20: ";
+			cin >> num;
+		}
+
+		cout<<"The factorial of "<<num<<" is "<<factorial(num)<<"\n";
+		cout<<"Enter 'Y' to find another factorial.\n";
+		cout<<"Or press 'N' to exit. ";
+		cin >> repeat;
+
+	} while (toupper(repeat) == 'Y');
+	
+
 
 	return 0;
 }
