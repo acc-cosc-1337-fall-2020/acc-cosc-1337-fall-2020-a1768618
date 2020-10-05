043406727866e648c29b4d578f6b0122a8b3c92d
a1768618 <abner.herrera-chinea@g.austincc.edu>
2020-10-05 16:52:05 -0500

    vectors and vectors test

--- src/examples/04_module/06_vectors/vec.cpp
+++ src/examples/04_module/06_vectors/vec.cpp
@@ -4,24 +4,61 @@
 
 using std::vector;
 
-
-
-/*
-Function loop_vector_w_index 
-The function creates an int vector with values 9, 10, 99, 5, 67
-and displays the numbers to screen using an indexed for loop as follows: 9 10 99 5  67
-
-@param none*/
-
 void loop_string_w_index(std::string str)
 {
     for(std::size_t i=0; i < str.size(); ++i)
     {
         str[i] = 'z';
     }
-
 }
 
+void loop_string_w_index_ref(std::string& str)
+{
+    for(std::size_t i=0; i < str.size(); ++i)
+    {
+        str[i] = 'z';
+    }    
+}
+
+void loop_string_for_ranged(std::string str)
+{
+    for(auto c: str)
+    {
+        c = 'y';
+    }
+}
+
+void loop_string_for_ranged_ref(std::string& str)
+{
+    for(auto c: str)//value target variable
+    {
+        c = 'y';
+    }
+}
+
+void loop_string_for_ranged_ref_fr_ref(std::string& str)
+{
+    for(auto& c: str)//reference target variable
+    {
+        c = 'y';
+    }
+}
+
+/*
+Function loop_vector_w_index 
+The function creates an int vector with values 9, 10, 99, 5, 67
+and displays the numbers to screen using an indexed for loop as follows: 9 10 99 5  67
+
+@param none
+
+*/
+void loop_vector_w_index(std::vector<int> nums)
+{
+    for(std::size_t i = 0; i < nums.size(); ++i)
+    {
+        nums[i] = 0;
+    }
+}
 
 
 
@@ -32,14 +69,6 @@ Displays the incoming parameter vector numbers to screen using an indexed for lo
 @param vector<int>
 */
 
-void loop_string_w_index_ref(std::string& str)
-{
-    for(std::size_t i=0; i < str.size(); ++i)
-    {
-        str[i] = 'z';
-    }
-
-}
 
 /*
 Function loop_vector_w_index_ref
@@ -47,7 +76,37 @@ Displays the incoming parameter vector numbers to screen using an indexed for lo
 
 @param vector<int>&
 */
+void loop_vector_w_index_ref(std::vector<int>& nums)
+{
+    for(std::size_t i = 0; i < nums.size(); ++i)
+    {
+        nums[i] = 0;
+    }    
+}
 
+void loop_vector_for_ranged(std::vector<int> nums)
+{
+    for(auto n: nums)
+    {
+        n = 0;
+    }
+}
+
+void loop_vector_for_ranged_ref(std::vector<int>& nums)
+{
+    for(auto n: nums)//value target variable
+    {
+        n = 0;
+    }
+}
+
+void loop_vector_for_ranged_ref_fr_ref(std::vector<int>& nums)
+{
+    for(auto& n: nums)//reference target variable
+    {
+        n = 0;
+    }
+}
 
 /*
 Write code for function loop_vector_w_index_const_ref
--- src/examples/04_module/06_vectors/vec.h
+++ src/examples/04_module/06_vectors/vec.h
@@ -1,10 +1,28 @@
 #include<string>
 #include<vector>
 
+using std::vector;
+using std::string;
+
 void loop_string_w_index(std::string str);
+
 void loop_string_w_index_ref(std::string& str);
+
 void loop_string_for_ranged(std::string str);
+
 void loop_string_for_ranged_ref(std::string& str);
+
+void loop_string_for_ranged_ref_fr_ref(std::string& str);
+
+void loop_vector_w_index(std::vector<int> nums);
+
+void loop_vector_w_index_ref(std::vector<int>& nums);
+
+void loop_vector_for_ranged(std::vector<int> nums);
+
+void loop_vector_for_ranged_ref(std::vector<int>& nums);
+
+void loop_vector_for_ranged_ref_fr_ref(std::vector<int>& nums);
 
 /*
 Write function prototype for void function loop_vector_w_index with no parameters.
