66165b0188a7542ed7a4421a0b586b2e9e60d9ff
a1768618 <abner.herrera-chinea@g.austincc.edu>
2020-11-18 13:21:40 -0600

    File read and write w fstream objects

--- src/classwork/06_assign/main.cpp
+++ src/classwork/06_assign/main.cpp
@@ -1,5 +1,7 @@
 //write include statemetns
-
+#include<iostream>
+#include<fstream>
+#include<string>
 /*
 Create a vector of rectangles
 Add 3 Rectangle classes to the vector:
@@ -12,6 +14,36 @@ Iterate the vector and display the Area for each Rectangle on one line and the t
 */
 int main()
 {
-	
+	std::ofstream out_file;
+
+  std::cout<<"Open file: \n";
+  out_file.open("names.dat");
+
+  out_file<<"joe"<<std::endl;
+  out_file<<"mary"<<std::endl;
+  out_file<<"john"<<std::endl;
+
+  std::cout<<"Close file\n";
+
+  //READ FILE
+
+  std::ifstream in_file;
+  std::cout<<"\n open file\n\n";
+  in_file.open("names.dat");
+
+  std::string name;
+  std::cout<<"Names";
+
+  if(in_file.is_open())
+  {
+    std::cout<<"read: ";
+    while(std::getline(in_file, name))
+    {
+      std::cout<<"name: "<<name<<"\n";
+    }
+
+  }
+  
+  in_file.close();
 	return 0;
 }
