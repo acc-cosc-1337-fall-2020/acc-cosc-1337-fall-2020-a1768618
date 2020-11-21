870eaf3ae00115707db24ee9dea0f2112cf82fc5
a1768618 <abner.herrera-chinea@g.austincc.edu>
2020-11-20 18:48:48 -0600

    arrays

--- src/examples/10_module/01_arrays_mem/arrays_mem.h
+++ src/examples/10_module/01_arrays_mem/arrays_mem.h
@@ -1,3 +1,16 @@
+#include<iostream>
+#include<string>
 //arrays_mem.h - stack array example
 
 //function prototype for stack_array
+void stack_array();
+
+void stack_array_cin();
+
+void stack_array_strings();
+
+void month_and_days();
+
+void array_partial_initialization();
+
+void array_for_ranged(); 
