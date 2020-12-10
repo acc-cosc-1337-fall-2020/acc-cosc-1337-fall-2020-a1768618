715da441a1f6a1cbd214649db01951e2417cd327
a1768618 <abner.herrera-chinea@g.austincc.edu>
2020-12-10 11:57:27 -0600

    recursion

--- src/examples/13_module/01_recursion/main.cpp
+++ src/examples/13_module/01_recursion/main.cpp
@@ -2,6 +2,16 @@
 
 int main() 
 {
-		
+	/*
+	std::cout<<"Load stack\n";
+	display(3);
+	std::cout<<"Unload stack\n";
+	*/
+
+	std::cout<<"load the stack 5\n";
+	int f = factorial(5);
+	std::cout<<"unload stack 5\n\n";
+	std::cout<<"factorial: "<<f<<"\n";
+	
 	return 0;
 }
--- src/examples/13_module/01_recursion/recursion.cpp
+++ src/examples/13_module/01_recursion/recursion.cpp
@@ -1,6 +1,34 @@
 #include "recursion.h"
 #include<iostream>
 //Write code for recursive display function
+void display(int count)
+{
+    int c;
+    if(count==0) // The base case
+    {
+        std::cout<<"Base Case - Start unloading the stack\n";
+        return; // start unloading/unwinding the stack
+    }
 
+    std::cout<<"hello-load stack "<<count-1<<"\n";
+    c = count-1;
+    display(c);
+    std::cout<<"unload stack\n";
+}
 
 //Write code for recursive factorial
+int factorial(int n)
+{
+    int f; // track the rturn value of the factorial
+    int r; // track the recursive function call return value
+    if(n == 0)
+    {
+        return 1;
+    }
+    std::cout<<"load stack "<<n-1<<"\n";
+    r=factorial(n-1);
+    f=n*r;
+    std::cout<<"unload stack n: "<<n<<"r: "<<f<<"\n";
+
+    return f;
+}
