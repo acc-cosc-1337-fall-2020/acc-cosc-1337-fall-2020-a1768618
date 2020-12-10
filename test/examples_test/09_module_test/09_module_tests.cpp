791b65c60baab94c2bd854142454d42363248bb1
a1768618 <abner.herrera-chinea@g.austincc.edu>
2020-12-09 19:50:40 -0600

    myvector: constructor, copy cons/assign, destr

--- test/examples_test/09_module_test/09_module_tests.cpp
+++ test/examples_test/09_module_test/09_module_tests.cpp
@@ -1,10 +1,12 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "ref_pointers.h"
+#include "vector.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
+
 
 TEST_CASE("Reference and pointer function parameters")
 {
@@ -16,13 +18,32 @@ TEST_CASE("Reference and pointer function parameters")
 	REQUIRE(n2 == 200);
 }
 
-TEST_CASE("Reference and pointer function parameters use a pointer")
+
+TEST_CASE("Test create new Vector from an existing vector instance check size")
 {
-	int n1 = 5, n2 = 10;
-	int* ptr = &n2;
+	Vector v1(3);
+    Vector v2 = v1;
 
-	ref_param(n1, ptr);
-
-	REQUIRE(n1 == 100);
-	REQUIRE(n2 == 200);
+	REQUIRE(v1.Size() == v2.Size());
 }
+
+TEST_CASE("Test create new Vector from an existing vector instance check elements")
+{
+	Vector v1(3);
+    Vector v2 = v1;
+	v1[0] = 5;
+
+	REQUIRE(v1[0] != v2[0]);
+}
+
+TEST_CASE("Test overwrite new Vector from an existing vector instance check elements")
+{
+	Vector v3(3);
+	Vector v4(3);
+
+    v4 = v3;
+	v3[0] = 5;
+
+	REQUIRE(v4[0] != v3[0]);
+}
+
